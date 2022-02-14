5e939c0cc20e71ca514fa68a4d1b49eca7a10dbd
Henrique Pompeo Modesto <Henrique.PompeoModesto-ext@ab-Inbev.com>
2022-02-14 18:36:07 -0300

    Implemented getbrewerylist unit test success case

--- features/brewerylist/src/test/java/com/henrique/brewerylist/ui/BreweryListViewModelTest.kt
+++ features/brewerylist/src/test/java/com/henrique/brewerylist/ui/BreweryListViewModelTest.kt
@@ -6,8 +6,14 @@ import com.henrique.brewerylist.BreweryListTest
 import com.henrique.brewerylist.data.repository.BreweryListRepository
 import com.henrique.shared.data.Result
 import com.henrique.shared.domain.model.Brewery
+import io.kotlintest.matchers.exactly
 import io.kotlintest.shouldBe
-import io.mockk.*
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
@@ -16,8 +22,8 @@ import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.Before
 import org.junit.Rule
-import org.junit.Assert
 import org.junit.After
+import org.junit.Assert
 import org.junit.Test
 import org.junit.rules.TestRule
 import org.koin.core.component.KoinApiExtension
@@ -30,7 +36,9 @@ class BreweryListViewModelTest : BreweryListTest() {
 
     private lateinit var breweryListViewModel: BreweryListViewModel
 
-    private val breweryListObserver = mockk<Observer<Result<List<Brewery>>>>()
+    private val breweryListObserver = mockk<Observer<Result<List<Brewery>>>>(relaxed = true)
+
+    private val breweryListStates = mutableListOf<Result<List<Brewery>>>()
 
     private val breweryListRepository = mockk<BreweryListRepository>()
 
@@ -52,30 +60,33 @@ class BreweryListViewModelTest : BreweryListTest() {
     fun setup() {
         with(Dispatchers) { setMain(testDispatcher) }
 
-        coEvery { breweryListRepository.getBreweryList() } returns listOf(brewery)
-
-        breweryListViewModel = BreweryListViewModel(breweryListRepository)
+        breweryListViewModel = BreweryListViewModel(
+            breweryListRepository = breweryListRepository
+        )
     }
 
     @Test
-    fun `SHOULD call getBreweryList() and THEN post success WHEN the call succeeds`() = runBlocking {
+    fun `SHOULD call getBreweryList() and THEN post success WHEN the call succeeds`() =
+        runBlocking {
 
-        with(breweryListViewModel) {
-            breweryList.observeForever(breweryListObserver)
-            getBreweryList()
-            breweryList.value.let {
-                Assert.assertEquals(it, Result.Success(listOf(brewery)))
+            coEvery { breweryListRepository.getBreweryList() } returns listOf(brewery)
+
+            with(breweryListViewModel) {
+                breweryList.observeForever(breweryListObserver)
+                getBreweryList()
             }
+
+            coVerify { breweryListRepository.getBreweryList() }
+
+            verify { breweryListObserver.onChanged(capture(breweryListStates)) }
+
+            breweryListStates shouldBe listOf(
+                Result.Loading,
+                Result.Success(listOf(brewery))
+            )
+
+            confirmVerified(breweryListRepository)
         }
-
-        coVerify(exactly = 1) { breweryListRepository.getBreweryList() }
-
-        // TODO - no answer found for: Observer(#1).onChanged(com.henrique.shared.data.Result$Loading@62da83ed)
-//        verify { breweryListObserver.onChanged(Result.Success(listOf(brewery))) }
-//        Result.Success(listOf(brewery)).data shouldBe listOf(brewery)
-
-        confirmVerified(breweryListRepository)
-    }
 
     @Test
     fun `SHOULD Call getBreweryList() and THEN post error WHEN the call fails`() = runBlocking {
