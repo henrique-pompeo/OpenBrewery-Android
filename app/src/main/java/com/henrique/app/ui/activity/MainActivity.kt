51f38e5b950ab196528a1d66e0234797b86a3fb9
Henrique Pompeo Modesto <h.modesto@animalltag.com.br>
2022-03-27 17:08:29 -0300

    Added new style gradient bottom toolbar

--- app/src/main/java/com/henrique/app/ui/activity/MainActivity.kt
+++ app/src/main/java/com/henrique/app/ui/activity/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         navController = navHostFragment.navController
 
-        setupActionBarWithNavController(navController)
+        //setupActionBarWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
